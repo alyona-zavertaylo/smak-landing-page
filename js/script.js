4293144532c051c7ed8e238777ca79512ab2c029
Alyona-Zavertaylo <alena.zavertaylo@gmail.com>
2019-02-23 22:25:05 +0200

    initial commit of smak-landing-page

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+  /*
+    ========================================
+   Scrollit.js
+    ========================================
+  */
+  //  $(function() {
+  //   $.scrollIt();
+  // });
+
+  // Slider
+
+  // const buttonLeft = document.querySelector('slider-controls .left');
+
+  /*
+    ========================================
+   Progress bar - easyPieChart
+    ========================================
+  */
+
+  $(function() {
+    $('.chart').easyPieChart({
+      siza: 180,
+      barColor: '#ffe600',
+      scaleColor: false,
+      lineWidth: 6,
+      trackColor: '#fff',
+      lineCap: 'circle',
+      animate: 2000
+    })
+  })
+
+
+  
+};
